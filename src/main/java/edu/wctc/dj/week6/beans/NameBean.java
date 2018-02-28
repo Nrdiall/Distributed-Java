065c4896389bdbc8f467b31fce220b598f863d0d
L117student <L117student@L117-05-042768.bitnet.wctc.edu>
2018-02-27 19:35:28 -0600

    This is the week 6 in class stuff

---
+++ src/main/java/edu/wctc/dj/week6/beans/NameBean.java
@@ -0,0 +1,27 @@
+package edu.wctc.dj.week6.beans;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author L117student
+ */
+@Named(value = "nameBean")
+@SessionScoped
+public class NameBean implements Serializable {
+
+    /**
+     * Creates a new instance of NameBean
+     */
+    public NameBean() {
+    }
+    
+}
